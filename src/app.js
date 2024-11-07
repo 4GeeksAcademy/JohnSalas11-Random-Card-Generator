cf0f417f775b8ab1d94af940a650cddb9bf3e211
John <salasduranjohn96@gmail.com>
2024-11-07 18:30:57 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let palos = ["♠", "♥", "♣", "♦"];
+  let valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let palosRandom = palos[Math.floor(Math.random() * palos.length)];
+  let valoresRandom = valores[Math.floor(Math.random() * valores.length)];
+
+  let iconoArriba = document.querySelector("#iconoArriba");
+  let iconoAbajo = document.querySelector("#iconoAbajo");
+  let valor = document.querySelector("#valor");
+
+  iconoArriba.textContent = palosRandom;
+  iconoAbajo.textContent = palosRandom;
+  valor.textContent = valoresRandom;
+
+  if (palosRandom === "♥" || palosRandom === "♦") {
+    iconoArriba.style.color = "red";
+    iconoAbajo.style.color = "red";
+  } else {
+    iconoArriba.style.color = "black";
+    iconoAbajo.style.color = "black";
+  }
 };
